f7e954f12f01bdc835937460a8ca0d15451ab72a
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2023-05-10 12:30:38 +0100

    setup library pixi.js

--- pixi and gsap/app.js
+++ pixi and gsap/app.js
@@ -1,8 +1,27 @@
 PIXI.utils.sayHello(); // Confrims PIXI is working
 
-var renderer = PIXI.autoDetectRenderer(512, 512, {
+// Creates something on the canvas for Pixi to add stuff to, called a renderer.
+var renderer = PIXI.autoDetectRenderer(512, 512, { 
     transparent: true,
     resolution: 1
 }); 
 
 document.getElementById('display').appendChild(renderer.view);
+
+var stage = new PIXI.Container(); // Sets up a stage for pixi to put stuff on
+
+var loader = new PIXI.loader
+    .add("images/FileReader.png")
+    .load(setup);
+
+var fire;
+
+function setup(){
+    fire = new PIXI.sprite(
+        PIXI.loader.resource("fire").texture
+    );
+    stage.addChild(fire);
+    renderer.render(stage); // Adds it to the rendeer cavas element
+}
+
+
