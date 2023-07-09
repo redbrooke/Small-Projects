da6700eb447dcdea24736e28fd0c163e1c680ae6
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2023-07-09 10:55:45 +0100

    added dvd stuff

--- pixi and gsap/DVD screen/app.js
+++ pixi and gsap/DVD screen/app.js
@@ -1,7 +1,14 @@
 const app = new PIXI.Application({background: '#FFFFFF', height:window.innerHeight, width:window.innerWidth});
 document.body.appendChild(app.view);
 
+originX = 0;
+originY = 0;
+targetX = app.screen.height;
+targetY = app.screen.width + 1;
+
+
 const backgroundTexture = PIXI.Texture.from('./images/static.gif');
+const dvdLogo = PIXI.Texture.from('./images/DVD.png');
 
 /**
  * The position of sprites depends on what order the child objects are added
@@ -11,3 +18,24 @@ const background    =  new PIXI.Sprite(backgroundTexture);
 background.width    = app.screen.width;
 background.height   = app.screen.height;
 app.stage.addChild(background);
+
+const DVD           = new PIXI.Sprite(dvdLogo);
+DVD.width           = app.screen.width / 6;
+DVD.height          = app.screen.width / 6;
+app.stage.addChild(DVD);
+
+while (true){
+    adjustTragectory();
+}
+
+function adjustTragectory(){
+
+    originX = targetX;
+    originY = targetY;
+
+    if(targetX == app.screen.height){};
+    if(targetY == app.screen.width){};
+
+
+    gsap.fromTo(DVD, {duration:1, y:originY, x:originX}, {duration:1, y:targetY, x:targetX});
+};
