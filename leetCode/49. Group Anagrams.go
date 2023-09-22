ce91ab15874e0fffd3d587139cc5fde194bf3068
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2023-09-22 20:02:53 +0100

    Create 49. Group Anagrams.go

---
+++ leetCode/49. Group Anagrams.go
@@ -0,0 +1,15 @@
+// INCOMPLETE
+func groupAnagrams(strs []string) [][]string {
+    
+    map := make(map[string]int);
+    // Map used for: organized phrase, index in return array.
+
+    for i := 0; i < len(strs); i++ {
+        // Organize string into same order
+        for nextChar :=0; nextChar < len(strs[i]); nextChar++ {
+            
+        } 
+        // Add to index
+    }
+
+}
