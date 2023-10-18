55fc311d0555119cb198e8770e00cd0597ec8cc0
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2023-10-18 11:31:16 +0100

    added execution

--- go_prototype/returnedInstruction.go
+++ go_prototype/returnedInstruction.go
@@ -1,6 +1,29 @@
+import (
+   "fmt"
+   "log"
+   "os/exec"
+)
+
 type returnedInstruction struct {
   command string
   success bool
   err error
   output string
+}
+
+func (command returnedInstruction) execute() string,int {
+	cmd := exec.Command()
+  cmd := exec.Command("powershell", "-nologo", "-noprofile")
+  stdin, err := cmd.StdinPipe()
+  if err != nil {
+      log.Fatal(err)
+  }    
+  defer stdin.Close()
+  fmt.Fprintln(stdin, command.command)
+  out, err := cmd.CombinedOutput()
+  if err != nil {
+      log.Fatal(err)
+  }
+  fmt.Printf("%s\n", out)
+  return out, err
 }
