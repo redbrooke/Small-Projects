b2020ba2511baf912695930513fdf45fd68a0c05
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2023-10-19 12:58:25 +0100

    set up go env

--- go_prototype/main.go
+++ go_prototype/main.go
@@ -1,12 +1,8 @@
 package main
 
-import {
-  instruction "returnedInstruction"
-}
-
 func main() {
-  string exampleCommand = "whoami";
-  instruction.setCommand(exampleCommand);
-  out, err = instruction.execute();
+	var exampleCommand := "whoami"
+	instruction.setCommand(exampleCommand)
+	out, err = instruction.execute()
+	fmt.println("hello")
 }
-
