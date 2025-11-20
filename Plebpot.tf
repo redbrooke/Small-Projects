8d6af6aab8a9903ac2080ee275d99b7f8b6c7873
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2025-11-20 18:51:38 +0000

    Update Plebpot.tf

--- Plebpot.tf
+++ Plebpot.tf
@@ -103,3 +103,21 @@ output "honeypot_public_ip" {
   value = azurerm_public_ip.honeypot_ip.ip_address
 
 }
+
+### Sentinel
+
+resource "azurerm_log_analytics_datasource_linux_syslog" "syslog_auth" {
+  name                = "syslog-auth"
+  resource_group_name = azurerm_resource_group.honeypot_rg.name
+  workspace_name      = azurerm_log_analytics_workspace.honeypot_law.name
+  syslog_facilities   = ["auth", "authpriv", "syslog", "cron", "kern"]
+  syslog_levels       = ["Debug", "Info", "Notice", "Warning", "Error", "Critical", "Alert", "Emergency"]
+}
+
+resource "azurerm_virtual_machine_extension" "oms_agent" {
+  name                       = "OmsAgentForLinux"
+  virtual_machine_id         = azurerm_linux_virtual_machine.honeypot_vm.id
+  publisher                  = "Microsoft.EnterpriseCloud.Monitoring"
+  type                       = "OmsAgentForLinux"
+  type_handler_version       = "1.13"
+  auto_upgrade_minor_version = true
